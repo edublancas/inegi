4d59cf41b7d6ac83a7a59c14b9f01c05aa6c05de
Eduardo Blancas Reyes <edu@cacaolabs.co>
2015-06-16 17:53:15 -0500

    script now shows if option is file or directory

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 #Descarga de DENUE (INEGI)
 Este programa permite descargar archivos del DENUE desde la página del INEGI. Actualmente es funcional pero se tiene que mejorar la interacción con el usuario y permitir descargar secciones completadas.
+
+http://www3.inegi.org.mx/sistemas/descarga/
--- script.py
+++ script.py
@@ -8,39 +8,56 @@ from bs4 import BeautifulSoup
 from ghost import Ghost
 import re, urllib
 
+#Generate table for options
 def printOptions(ops):
-	rows = [str(idx)+'\t'+s for idx,s in enumerate(ops)]
+	rows = [str(idx)+'\t'+dic["name"]+'\t'+dic["type"] for idx,dic in enumerate(ops)]
 	for row in rows:
 		print row
 
+#Print file names
 def printFilenames(filenames):
 	for name in filenames:
 		print name
 
+#Get names and URLs from file listing
 def getNamesAndUrlsFromSoup(soup):
 	links = soup.find_all("a", { "id" : re.compile("GV_Datos_ctl.._lnkArchivo") })
 	names = [link.getText() for link in links]
 	urls = [link['href'] for link in links]
-	return {'names':names, 'urls':urls}
+	rows = soup.find_all("tr", { "class" : "TdCenso"})
+	types = [list(row.children)[3].string for row in rows]
+	#return {'names':names, 'urls':urls, 'types':types}
+	return [{"name":a[0], "url":a[1], "type":a[2]} for a in zip(names,urls,types)]
 
-
-#Secciones
+#Open INEGI massive download website
 ghost = Ghost(wait_timeout=20)
-url = 'http://www3.inegi.org.mx/sistemas/descarga/?c=200'
+url = 'http://www3.inegi.org.mx/sistemas/descarga/'
 ghost.open(url)
 soup = BeautifulSoup(ghost.content)
+
+#Parse sections
 res_sections = getNamesAndUrlsFromSoup(soup)
+#Print sections
+printOptions(res_sections)
 
-printOptions(res_sections['names'])
+#Ask for a section number
 section = input('Selecciona una sección: ')
+#Check if value is valid
+while not section in range(len(res_sections)):
+	section = input('Opción incorrecta. Selecciona una sección: ')
 
-#Subsecciones
+#Scrape subsections
 ghost.evaluate(res_sections['urls'][section], expect_loading=True) #Cargar alguna de las opciones
 soup = BeautifulSoup(ghost.content)
 res_subsections = getNamesAndUrlsFromSoup(soup)
 
+#Print subsections
 printOptions(res_subsections['names'])
+
 subsection = input('Selecciona una subsección: ')
+#Check if value is valid
+while not subsection in range(len(res_subsections)):
+	subsection = input('Opción incorrecta. Selecciona una subsección: ')
 
 
 #Carga listado de archivos
